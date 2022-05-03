55e9b68650a86aa144bc6c5e6595646ab04176ef
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-03 23:16:47 +0600

    fix: exceptions

--- PyMig/Connect.py
+++ PyMig/Connect.py
@@ -1,25 +1,31 @@
 import mysql.connector
+from utils import MainUtils
 
 
 class Connect:
-    def __init__(self, host:str='localhost', user:str='root', password:str='', database:str=''):
+    def __init__(self, host: str = 'localhost', user: str = 'root', password: str = '', database: str = ''):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
- 
+
     def connect(self):
-        conn = mysql.connector.connect( 
-            host=self.host, 
-            user=self.user, 
+        conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
             password=self.password
         )
         return conn
-    
+
     def connect_db(self):
-        conn  = mysql.connector.connect(self.host, self.user, self.password, self.database)
-        cursor = conn.cursor()
-        return cursor
+        try:
+            conn = mysql.connector.connect(
+                host=self.host, user=self.user, password=self.password, database=self.database)
+
+            cursor = conn.cursor()
+            return cursor
+        except:
+            MainUtils.box_line("connect db: Something went wrong.")
 
     def show_database(self):
         db = self.connect()
@@ -29,4 +35,3 @@ class Connect:
         for i in cursor:
             arr.append(i[0])
         return arr
-
