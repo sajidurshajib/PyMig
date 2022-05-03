6bf57cccd39fb4f21617d37c6e3b36eb9a1132eb
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-03 21:33:26 +0600

    created: utils for str_manp

---
+++ utils/main_utils.py
@@ -0,0 +1,6 @@
+class MainUtils:
+    @staticmethod
+    def box_line(line):
+        print("\n +"+"-"*(len(line)+2)+"+")
+        print(" | "+line+" |")
+        print(" +"+"-"*(len(line)+2)+"+\n")
